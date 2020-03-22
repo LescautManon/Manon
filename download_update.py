6c761a802d49205ccacb6d5776cdefa302e3c118
LescautManon <svit.niky@gmail.com>
2020-03-22 07:55:51 +0300

    commit

--- download_update.py
+++ download_update.py
@@ -2,7 +2,7 @@ from urllib.request import urlretrieve
 from urllib.request import urlopen
 from urllib.error import URLError
 
-version_main = 15
+version_main = 1
 version_database = 0
 version_download_update = 0
 list_update = []
@@ -29,6 +29,8 @@ def update():
     if len(list_update) == 0:
         print ("Возможно, нет подключения к интернету")
         return
+    else:
+        import re
     if list_update[0] > version_main:
         url = 'https://raw.githubusercontent.com/LescautManon/Manon/master/main.py'
         filename = "main.py"
@@ -36,7 +38,6 @@ def update():
         print('update main.py OK')
         old_version = version_main
         new_version = list_update[0]
-        import re
         data = open('download_update.py').read()
         o = open('download_update.py','w')
         o.write(re.sub(f"version_main = {old_version}", f"version_main = {new_version}", data))
@@ -51,10 +52,22 @@ def update():
         print('update mydatabase.db OK')
         old_version = version_database
         new_version = list_update[1]
-        import re
         data = open('download_update.py').read()
         o = open('download_update.py','w')
         o.write(re.sub(f"version_database = {old_version}", f"version_database = {new_version}", data))
+        o.close()
+        counter_updates += 1
+
+    if list_update[2] > version_download_update:
+        url = 'https://raw.githubusercontent.com/LescautManon/Manon/master/download_update.py'
+        filename = "download_update.py"
+        urlretrieve(url, filename)
+        print('update download_update.py OK')
+        old_version = version_download_update
+        new_version = list_update[2]
+        data = open('download_update.py').read()
+        o = open('download_update.py','w')
+        o.write(re.sub(f"version_download_update = {old_version}", f"version_download_update = {new_version}", data))
         o.close()
         counter_updates += 1
 
